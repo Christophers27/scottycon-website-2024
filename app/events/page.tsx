dd5b01e9fffd8acc323e0577f42aeeac5c16a00a
Christopher S. <christophersetiabudi@gmail.com>
2025-02-21 02:55:29 -0500

    calendar added

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -5,15 +5,14 @@ import SectionHeader from "@/components/sectionHeader";
 import EventCard from "@/components/eventCard";
 import { events } from "@/lib/data";
 
-import { BiChevronDown } from "react-icons/bi";
-
 export default function EventsPage() {
   const [search, setSearch] = useState("");
-  const [collapsedTypes, setCollapsedTypes] = useState<string[]>(["Activity", "Food", "Panel", "Performance"]);
 
-  const groupByType = events.reduce(
+  // Group events by start time, every 30 minutes
+  const groupByTime = events.reduce(
     (groups: Record<string, typeof events>, event) => {
-      const key = event.type;
+      const mm = event.startTime.split(":")[1] < "30" ? "00" : "30";
+      const key = `${event.startTime.split(":")[0]}:${mm}`;
       if (!groups[key]) {
         groups[key] = [];
       }
@@ -23,64 +22,42 @@ export default function EventsPage() {
     {}
   );
 
-  function handleSearch(searchTerm: string) {
-    setSearch(searchTerm);
-  }
-
-  function handleCollapse(type: string) {
-    if (collapsedTypes.includes(type)) {
-      setCollapsedTypes(collapsedTypes.filter((t) => t !== type));
-    } else {
-      setCollapsedTypes([...collapsedTypes, type]);
-    }
-  }
-
   return (
     <div className="flex flex-col flex-1 bg-scottycon-background rounded-t-xl p-8 text-scottycon-text items-center">
       <SectionHeader>Events</SectionHeader>
-
       <input
         type="text"
         placeholder="Search by event name"
-        onChange={(e) => handleSearch(e.target.value)}
+        onChange={(e) => setSearch(e.target.value)}
         className="bg-scottycon-foreground m-4 px-8 py-4 rounded-full outline-none shadow-lg"
       />
-
-      <ul>
-        {Object.entries(groupByType).map(([type, events]) => {
-          return (
-            <li
-              key={type}
-              className="bg-scottycon-foreground/[0.6] p-4 rounded-xl my-4"
-            >
-              <div
-                className="flex gap-2 active:bg-scottycon-foreground/[0.8] transition rounded-full w-min px-4 justify-center items-center"
-                onClick={() => handleCollapse(type)}
-              >
-                <h2 className="text-2xl font-bold">{type}</h2>
-                <BiChevronDown
-                  className={`size-6 transform ${
-                    collapsedTypes.includes(type) && "rotate-180"
-                  }`}
-                />
+      <div className="flex flex-col w-full max-w-[45rem] h-[60svh] rounded-xl bg-scottycon-foreground p-2 overflow-auto">
+        {Object.keys(groupByTime)
+          .sort()
+          .map((time) => (
+            <div key={time} className="flex flex-col">
+              <div className="flex bg-scottycon-background/75 rounded-full">
+                <h2 className="basis-1/5 flex flex-col items-end font-semibold text-xl px-1">
+                  {time}
+                </h2>
+                <div className="flex basis-4/5 h-full" />
               </div>
-              {!collapsedTypes.includes(type) && (
-                <ul>
-                  {events
-                    .sort((a, b) => a.startTime.localeCompare(b.startTime))
-                    .map((event) => {
-                      if (
-                        event.name.toLowerCase().includes(search.toLowerCase())
-                      ) {
-                        return <EventCard key={event.name} {...event} />;
-                      }
-                    })}
-                </ul>
-              )}
-            </li>
-          );
-        })}
-      </ul>
+              <div className="flex flex-col flex-1 w-full">
+                {groupByTime[time]
+                  .filter(
+                    (event) =>
+                      event.name.toLowerCase().includes(search.toLowerCase()) ||
+                      event.description
+                        .toLowerCase()
+                        .includes(search.toLowerCase())
+                  )
+                  .map((event) => (
+                    <EventCard key={event.name} {...event} />
+                  ))}
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
--- components/eventCard.tsx
+++ components/eventCard.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { ReactElement, useState } from "react";
 import { events } from "@/lib/data";
 import {
-  BiSolidMap,
-  BiSolidPurchaseTag,
-  BiSolidDetail,
-  BiSolidTimeFive,
-} from "react-icons/bi";
-import Image from "next/image";
+  BsFillGeoAltFill,
+  BsPersonWalking,
+  BsPersonVideo3,
+  BsMusicNote,
+} from "react-icons/bs";
+import { FaUtensils } from "react-icons/fa";
 
 type eventCardProps = (typeof events)[number];
 
@@ -21,59 +21,34 @@ export default function EventCard({
   location,
   img,
 }: eventCardProps) {
-  const [clicked, setClicked] = useState(false);
-
-  function handleClick() {
-    setClicked(!clicked);
-  }
+  const [expanded, setExpanded] = useState(false);
 
   return (
-    <div
-      className="bg-scottycon-foreground p-4 m-4 rounded-xl shadow-lg text-scottycon-text min-w-[20rem] max-w-[50rem]"
-      onClick={handleClick}
-    >
-      <h2 className="font-bold text-lg border-b border-scottycon-text mb-2">
-        {name}
-      </h2>
-
-      {!clicked && (
-        <>
-          <p className="">{type}</p>
-          <p className="">
-            {startTime} - {endTime} | {location}
-          </p>
-        </>
-      )}
-
-      {clicked && (
-        <>
-          <div className="flex gap-2 items-center">
-            <BiSolidPurchaseTag className="min-w-4" />
-            <p>{type}</p>
+    <div className="flex" onClick={() => setExpanded(!expanded)}>
+      <div className="flex flex-col basis-1/5 align-top items-end px-1 py-2">
+        <p className="font-semibold">{startTime}</p>
+        <p className="text-scottycon-text/50">{endTime}</p>
+      </div>
+      <div className="flex flex-col items-start basis-4/5 px-2 py-2 border-b border-black/10">
+        <h2 className="font-semibold">
+          {name}
+        </h2>
+        <p className="text-scottycon-text/50 text-sm flex items-center gap-1">
+          <BsFillGeoAltFill className="inline" /> {location}
+        </p>
+        <p className="text-scottycon-text/50 text-sm flex items-center gap-1">
+          {type === "Activity" && <BsPersonWalking className="inline" />}
+          {type === "Food" && <FaUtensils className="inline" />}
+          {type === "Panel" && <BsPersonVideo3 className="inline" />}
+          {type === "Performance" && <BsMusicNote className="inline" />}
+          {type}
+        </p>
+        {expanded && (
+          <div className="flex flex-col items-start pt-1">
+            <p className="text-scottycon-text text-sm">{description}</p>
           </div>
-          <div className="flex gap-2 items-center">
-            <BiSolidTimeFive className="min-w-4" />
-            <p>
-              {startTime} - {endTime}
-            </p>
-          </div>
-          <div className="flex gap-2 items-center">
-            <BiSolidMap className="min-w-4" />
-            <p>{location}</p>
-          </div>
-          <div className="flex gap-2">
-            <BiSolidDetail className="min-w-4 mt-1" />
-            <p className="whitespace-pre-wrap">{description}</p>
-          </div>
-          {img && (
-            <Image
-              src={img}
-              alt={name}
-              className="py-4 object-contain max-h-96"
-            />
-          )}
-        </>
-      )}
+        )}
+      </div>
     </div>
   );
 }
