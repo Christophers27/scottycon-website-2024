2841b2f012578f98377195e847be1db7307bae7f
Christopher S. <christophersetiabudi@gmail.com>
2025-02-21 03:10:37 -0500

    added filtering by event type

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -7,6 +7,7 @@ import { events } from "@/lib/data";
 
 export default function EventsPage() {
   const [search, setSearch] = useState("");
+  const [filterType, setFilterType] = useState("");
 
   // Group events by start time, every 30 minutes
   const groupByTime = events.reduce(
@@ -22,6 +23,8 @@ export default function EventsPage() {
     {}
   );
 
+  //Activity, Food, Panel, Performance
+
   return (
     <div className="flex flex-col flex-1 bg-scottycon-background rounded-t-xl p-8 text-scottycon-text items-center">
       <SectionHeader>Events</SectionHeader>
@@ -31,6 +34,50 @@ export default function EventsPage() {
         onChange={(e) => setSearch(e.target.value)}
         className="bg-scottycon-foreground m-4 px-8 py-4 rounded-full outline-none shadow-lg"
       />
+      <div className="flex flex-row gap-4 mb-4">
+        <button
+          className={`${
+            filterType === "Activity"
+              ? "bg-scottycon-text text-scottycon-foreground"
+              : "bg-scottycon-foreground"
+          } px-2 py-1 text-sm rounded-full shadow-lg`}
+          onClick={() =>
+            setFilterType("Activity" === filterType ? "" : "Activity")
+          }
+        >
+          Activity
+        </button>
+        <button
+          className={`${
+            filterType === "Food"
+              ? "bg-scottycon-text text-scottycon-foreground"
+              : "bg-scottycon-foreground"
+          } px-2 py-1 text-sm rounded-full shadow-lg`}
+          onClick={() => setFilterType("Food" === filterType ? "" : "Food")}
+        >
+          Food
+        </button>
+        <button
+          className={`${
+            filterType === "Panel"
+              ? "bg-scottycon-text text-scottycon-foreground"
+              : "bg-scottycon-foreground"
+          } px-2 py-1 text-sm rounded-full shadow-lg`}
+          onClick={() => setFilterType("Panel" === filterType ? "" : "Panel")}
+        >
+          Panel
+        </button>
+        <button
+          className={`${
+            filterType === "Performance"
+              ? "bg-scottycon-text text-scottycon-foreground"
+              : "bg-scottycon-foreground"
+          } px-2 py-1 text-sm rounded-full shadow-lg`}
+          onClick={() => setFilterType("Performance" === filterType ? "" : "Performance")}
+        >
+          Performance
+        </button>
+      </div>
       <div className="flex flex-col w-full max-w-[45rem] h-[60svh] rounded-xl bg-scottycon-foreground p-2 overflow-auto">
         {Object.keys(groupByTime)
           .sort()
@@ -46,10 +93,13 @@ export default function EventsPage() {
                 {groupByTime[time]
                   .filter(
                     (event) =>
-                      event.name.toLowerCase().includes(search.toLowerCase()) ||
-                      event.description
+                      (event.name
                         .toLowerCase()
-                        .includes(search.toLowerCase())
+                        .includes(search.toLowerCase()) ||
+                        event.description
+                          .toLowerCase()
+                          .includes(search.toLowerCase())) &&
+                      (filterType === "" || event.type === filterType)
                   )
                   .map((event) => (
                     <EventCard key={event.name} {...event} />
