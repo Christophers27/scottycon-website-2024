1cef371e3e49483b834cf60bf2a70b3457dd1d77
Christopher S. <christophersetiabudi@gmail.com>
2025-03-29 04:08:28 -0400

    update button

--- components/allowNotifications.tsx
+++ components/allowNotifications.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { useNotifications } from "@/context/notificationContext";
+import { BsBellFill, BsBellSlash } from "react-icons/bs";
 
 export default function AllowNotifications() {
   const { isSubscribed, registration, allowNotifications } = useNotifications();
-  if (isSubscribed || !registration) {
-    return <></>;
-  }
+  // if (isSubscribed || !registration) {
+  //   return <></>;
+  // }
 
   return (
     <>
@@ -13,8 +14,13 @@ export default function AllowNotifications() {
         type="button"
         onClick={allowNotifications}
         // disabled={!isSubscribed}
+        className="fixed top-0 left-0 z-50 m-4 p-2 bg-scottycon-pink/90 text-white rounded-full shadow-lg hover:scale-105 active:scale-95 transition"
       >
-        Allow Notifications
+        {isSubscribed || !registration ? (
+          <BsBellSlash className="text-2xl" />
+        ) : (
+          <BsBellFill className="text-2xl" />
+        )}
       </button>
     </>
   );
