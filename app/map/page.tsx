13b8e212b234d35fd099d88bcba455e295576b7a
Christopher S. <christophersetiabudi@gmail.com>
2025-03-29 01:03:17 -0400

    updated map

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function MapPage() {
   return (
     <main className="page h-[80dvh]">
-        <iframe src='https://maps.scottylabs.org/CUC' className='w-full h-full rounded-4xl border-2 border-black/10' loading='lazy' />
+        <iframe src='https://cmumaps-public.vercel.app' className='w-full h-full rounded-4xl border-2 border-black/10' loading='lazy' />
     </main>
   )
 }
